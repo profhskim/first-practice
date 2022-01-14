7f5ba294b4a902043234040224c52f945d6aba63
Haksoo Kim <profhskim2@gmail.com>
2022-01-14 16:06:18 +0900

    2022-01-14:16:06 commit

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -1,10 +1,10 @@
 const passport = require('passport') 
-const localStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
 const User = require('../models/user')
 module.exports = () => {
-    passport.use(new localStrategy({
+    passport.use(new LocalStrategy({
         usernameField: 'email',
         passwordField: 'password ',
     }, async (email, password, done) => {
--- routes/middlewares.js
+++ routes/middlewares.js
@@ -11,6 +11,6 @@ exports.isNotLoggedIn = (req, res, next) => {
         next()
     } else {
         const message = encodeURIComponent('로그인한 상태입니다.')
-        req.redirect(`/?error=${message}`)
+        res.redirect(`/?error=${message}`)
     }
 }
